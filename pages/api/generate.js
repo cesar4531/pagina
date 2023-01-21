66b3f96f5cbc21bb6d0e0b31a54bf9cb5d7e6c4b
cesar4531 <cesaradrianrs660@gmail.com>
2023-01-21 17:50:45 -0600

    Pagina de prueba

---
+++ pages/api/generate.js
@@ -0,0 +1,67 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const animal = req.body.animal || '';
+  if (animal.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid animal",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+  prompt: generatePrompt(animal),
+  temperature: 0,
+  max_tokens: 100,
+  top_p: 1,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.0,
+  stop: ["\n"],
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(animal) {
+  const capitalizedAnimal =
+    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  return `I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me a question that is nonsense, trickery, or has no clear answer, I will respond with "Unknown".
+
+  Q:Quien eres?
+  A: soy una inteligencia artifical creada por el 
+  TAET(Taller de Experimentos tecnologicos)
+
+Q: ${capitalizedAnimal}
+A:`;
+}
